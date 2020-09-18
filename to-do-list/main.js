74dc95765875b74a3d1a9d8fa7d81d119adc886f
Kevin <kevin.ka_96@hotmail.com>
2020-09-18 12:35:13 +0200

    Tiny fixes
    Tiny fixes on the CSS + CSS class for completed button

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -50,10 +50,10 @@ function deleteCheck(e)
         const todo = item.parentElement; 
         todo.remove();
     }
-    
+
     if (item.classList[0] === 'complete-btn')
     {
         const todo = item.parentElement;
-        todo.classList.toggle("completed");
+        todo.classList.toggle('completed');
     }
 }
